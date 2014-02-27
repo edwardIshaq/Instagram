d229122593ddd35ad639acb8a5ec08cb1e0658f2
Edward Ashak <edward.ishaq@gmail.com>
2014-02-27 14:31:58 -0800

    Add keys

--- Instagram/AppDelegate.h
+++ Instagram/AppDelegate.h
@@ -4,7 +4,15 @@
 //
 //  Created by Edward Ashak on 2/27/14.
 //  Copyright (c) 2014 Edward Ashak. All rights reserved.
-//
+// 
+
+/*
+ CLIENT ID	d46d6144433f456ca7016edc6b0c7e1f
+ CLIENT SECRET	4b80b757514e4d49ab868c70f2918904
+ WEBSITE URL	http://objective-coder.blogspot.com/
+ REDIRECT URI	http://objective-coder.blogspot.com/
+
+ */
 
 #import <UIKit/UIKit.h>
 
