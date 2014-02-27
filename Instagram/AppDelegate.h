8ca61138d7c9f0ca82b2e564c43972e477898012
Edward Ashak <edward.ishaq@gmail.com>
2014-02-27 14:28:08 -0800

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Edward Ashak on 2/27/14.
+//  Copyright (c) 2014 Edward Ashak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Instagram/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Instagram
+//
+//  Created by Edward Ashak on 2/27/14.
+//  Copyright (c) 2014 Edward Ashak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
